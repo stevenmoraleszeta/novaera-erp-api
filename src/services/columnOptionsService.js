21aa5c3350d38ab932598cd8b533e5955f19ba96
Dylanbt2227 <dylantorres2227@gmail.com>
2025-07-15 11:13:39 -0600

    Selection fix

--- src/services/columnOptionsService.js
+++ src/services/columnOptionsService.js
@@ -106,7 +106,7 @@ exports.getAvailableOptions = async (column_id) => {
   });
   
   // Si es una columna de foreign key, obtener opciones de la tabla referenciada
-  if (column.is_foreign_key && column.foreign_table_id && column.foreign_column_name) {
+  if (column.foreign_table_id && column.foreign_column_name) {
     console.log(`Getting options from foreign table ${column.foreign_table_id}, column ${column.foreign_column_name}`);
     
     const tableResult = await pool.query(
